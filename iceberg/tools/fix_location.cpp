9adb7c958760688a6cb099cecd1656d438b79f9b
Artem Zuikov <chertus@gmail.com>
2024-04-19 15:54:28 +0000

    X-117 fix_location in metadata log

--- iceberg/tools/fix_location.cpp
+++ iceberg/tools/fix_location.cpp
@@ -21,6 +21,28 @@ bool ReplacePattern(std::string& str, const std::string& from, const std::string
   return true;
 }
 
+std::string RemoveSuffix(const std::string& str, const std::string& suffix) {
+  if (suffix.empty()) {
+    return str;
+  }
+  if (str.ends_with(suffix)) {
+    return str.substr(0, str.size() - suffix.size());
+  }
+  return str;
+}
+
+std::string FileType(iceberg::ContentFile::FileContent content) {
+  switch (content) {
+    case iceberg::ContentFile::FileContent::kData:
+      return "data";
+    case iceberg::ContentFile::FileContent::kPositionDeletes:
+      return "del(pos)";
+    case iceberg::ContentFile::FileContent::kEqualityDeletes:
+      return "del(equal)";
+  }
+  return "unknown";
+}
+
 class MetadataTree {
  public:
   struct MetadataFile {
@@ -73,9 +95,17 @@ class MetadataTree {
     }
   }
 
-  void FixLocation(const std::string& new_location) {
+  std::map<int64_t, std::string> MetadataLog() const {
+    std::map<int64_t, std::string> out;
+    for (auto& log : medatada_file.table_metadata->metadata_log) {
+      out.emplace(log.timestamp_ms, log.metadata_file);
+    }
+    return out;
+  }
+
+  void FixLocation(const std::string& new_location, const std::string& suffix) {
     auto& metadata = medatada_file.table_metadata;
-    const std::string old_location = metadata->location;
+    const std::string old_location = RemoveSuffix(metadata->location, suffix);
     metadata->location = new_location;
 
     for (auto& snap : metadata->snapshots) {
@@ -104,6 +134,29 @@ class MetadataTree {
     return iceberg::ice_tea::WriteTableMetadataV2(*medatada_file.table_metadata, true);
   }
 
+  void WriteFiles(const std::filesystem::path& out_dir) const {
+    {
+      auto out_path = out_dir / medatada_file_path.filename();
+      std::string serialized = SerializeMetadataFile();
+      std::ofstream ofstream(out_path);
+      ofstream.write(serialized.data(), serialized.size());
+    }
+
+    for (auto& [path, man_list] : manifests_lists) {
+      auto out_path = out_dir / std::filesystem::path(path).filename();
+      std::string serialized = iceberg::ice_tea::WriteManifestList(man_list->manifests);
+      std::ofstream ofstream(out_path);
+      ofstream.write(serialized.data(), serialized.size());
+    }
+
+    for (auto& [path, man] : manifests) {
+      auto out_path = out_dir / std::filesystem::path(path).filename();
+      std::string serialized = iceberg::ice_tea::WriteManifestEntries(man->files);
+      std::ofstream ofstream(out_path);
+      ofstream.write(serialized.data(), serialized.size());
+    }
+  }
+
  private:
   std::filesystem::path medatada_file_path;
   MetadataFile medatada_file;
@@ -111,17 +164,38 @@ class MetadataTree {
   std::unordered_map<std::string, std::shared_ptr<Manifest>> manifests;
 
   std::filesystem::path FilesPath() const { return medatada_file_path.parent_path(); }
-};
 
-std::ostream& operator<<(std::ostream& os, const MetadataTree& meta_tree) {
-  os << meta_tree.SerializeMetadataFile() << std::endl;
-  return os;
-}
+  friend std::ostream& operator<<(std::ostream& os, const MetadataTree& meta_tree) {
+    os << "metadata " << meta_tree.medatada_file.table_metadata->location << "/" << meta_tree.medatada_file_path
+       << std::endl;
+#if 0
+    os << meta_tree.SerializeMetadataFile() << std::endl;
+#endif
+    for (auto& snap : meta_tree.medatada_file.table_metadata->snapshots) {
+      os << "snapshot seqno " << snap->sequence_number << " id " << snap->snapshot_id << " "
+         << snap->manifest_list_location << std::endl;
+    }
+    for (auto& [path, man_list] : meta_tree.manifests_lists) {
+      os << path << " [";
+      for (auto& man : man_list->manifests) {
+        os << man.path << ", ";
+      }
+      os << "]" << std::endl;
+    }
+    for (auto& [path, man] : meta_tree.manifests) {
+      for (auto& entry : man->files) {
+        os << FileType(entry.data_file.content) << " " << entry.data_file.file_path << std::endl;
+      }
+    }
+    return os;
+  }
+};
 
 }  // namespace
 
 ABSL_FLAG(std::string, metadata, "", "path to iceberg metadata JSON file");
 ABSL_FLAG(std::string, fix, "", "new location");
+ABSL_FLAG(std::string, suffix, "/iceberg", "suffix in location to keep");
 ABSL_FLAG(std::string, outdir, "", "path to dst");
 
 int main(int argc, char** argv) {
@@ -130,6 +204,7 @@ int main(int argc, char** argv) {
 
     const std::filesystem::path metadata_path = absl::GetFlag(FLAGS_metadata);
     const std::string fix = absl::GetFlag(FLAGS_fix);
+    const std::string suffix = absl::GetFlag(FLAGS_suffix);
     const std::filesystem::path outdir = absl::GetFlag(FLAGS_outdir);
 
     if (metadata_path.empty()) {
@@ -138,12 +213,32 @@ int main(int argc, char** argv) {
     }
 
     MetadataTree meta_tree(metadata_path);
-
-    std::unordered_map<std::string, iceberg::ManifestFile> manifests;
-    if (!fix.empty()) {
-      meta_tree.FixLocation(fix);
+    auto meta_log = meta_tree.MetadataLog();
+    std::vector<MetadataTree> prev_meta;
+    prev_meta.reserve(meta_log.size());
+    for (auto& [_, meta_file_path] : meta_log) {
+      auto path =
+          std::filesystem::absolute(metadata_path).parent_path() / std::filesystem::path(meta_file_path).filename();
+      prev_meta.emplace_back(MetadataTree(path));
     }
 
+    if (!fix.empty()) {
+      for (auto& prev_tree : prev_meta) {
+        prev_tree.FixLocation(fix, suffix);
+      }
+      meta_tree.FixLocation(fix, suffix);
+    }
+
+    if (!outdir.empty()) {
+      for (auto& prev_tree : prev_meta) {
+        prev_tree.WriteFiles(outdir);
+      }
+      meta_tree.WriteFiles(outdir);
+    }
+
+    for (auto& prev_tree : prev_meta) {
+      std::cout << prev_tree << std::endl;
+    }
     std::cout << meta_tree << std::endl;
   } catch (std::exception& ex) {
     std::cerr << ex.what() << std::endl;
