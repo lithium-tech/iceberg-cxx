780470ee2f7510e02fe663d1974b4d5963a75e03
Artem Zuikov <chertus@gmail.com>
2024-05-22 09:42:07 +0000

    X-139: syncice with own S3 client

---
+++ iceberg/tools/s3client.h
@@ -0,0 +1,245 @@
+#pragma once
+
+#include <arrow/api.h>
+#include <arrow/filesystem/localfs.h>
+#include <arrow/filesystem/s3fs.h>
+
+#include <cstdlib>
+#include <filesystem>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+namespace ice_tea {
+
+class S3Client {
+  static constexpr const char* AWS_ENDPOINT_URL = "AWS_ENDPOINT_URL";
+  static constexpr const char* AWS_DEFAULT_REGION = "AWS_DEFAULT_REGION";
+  static constexpr const char* AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID";
+  static constexpr const char* AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY";
+  static constexpr const char* AWS_EC2_METADATA_DISABLED = "AWS_EC2_METADATA_DISABLED";
+
+  static constexpr int64_t CHUNK_SIZE = 1024 * 1024;
+
+ public:
+  explicit S3Client(bool force) : continue_on_fail_(force) {
+    InitS3();
+
+    options_ = GetEnvOptions();
+    ClearEnvOptions();
+
+    fs_ = std::make_shared<arrow::fs::LocalFileSystem>();
+
+    auto s3fs_res = arrow::fs::S3FileSystem::Make(options_);
+    if (!s3fs_res.ok()) {
+      throw std::runtime_error(s3fs_res.status().ToString());
+    }
+    src_s3fs_ = *s3fs_res;
+
+    // TODO(chertus): another S3
+    dst_s3fs_ = *s3fs_res;
+  }
+
+  ~S3Client() {
+    if (!arrow::fs::IsS3Finalized()) {
+      arrow::fs::FinalizeS3().ok();
+    }
+  }
+
+  bool CopyFiles(const std::unordered_map<std::string, std::string>& renames, bool use_threads) {
+    std::unordered_set<std::string> found = CheckFiles(renames);
+
+    std::vector<arrow::fs::FileLocator> src;
+    std::vector<arrow::fs::FileLocator> dst;
+    src.reserve(renames.size());
+    dst.reserve(renames.size());
+
+    for (auto& [src_name, dst_name] : renames) {
+      arrow::fs::FileLocator from{.filesystem = src_s3fs_, .path = CropPrefix(src_name)};
+      if (from.path == src_name) {
+        from.filesystem = fs_;
+      }
+
+      if (!found.contains(from.path)) {
+        if (!continue_on_fail_) {
+          throw std::runtime_error(std::string("file not found ") + src_name);
+        }
+        continue;
+      }
+
+      arrow::fs::FileLocator to{.filesystem = dst_s3fs_, .path = CropPrefix(dst_name)};
+      if (to.path == dst_name) {
+        to.filesystem = fs_;
+      }
+
+      src.emplace_back(std::move(from));
+      dst.emplace_back(std::move(to));
+    }
+
+    auto status = arrow::fs::CopyFiles(src, dst, arrow::io::default_io_context(), CHUNK_SIZE, use_threads);
+    if (!status.ok()) {
+      throw std::runtime_error(status.ToString());
+    }
+    return true;
+  }
+
+  bool CopyDir(const std::string& src, const std::string& dst, bool use_threads) {
+    arrow::fs::FileSelector selector;
+    selector.allow_not_found = false;
+    selector.recursive = true;
+    selector.base_dir = CropPrefix(src);
+
+    auto src_fs = src_s3fs_;
+    if (selector.base_dir == src) {
+      src_fs = fs_;
+    }
+
+    auto dst_fs = dst_s3fs_;
+    auto dst_path = CropPrefix(dst);
+    if (dst_path == dst) {
+      dst_fs = fs_;
+    }
+
+    auto status = arrow::fs::CopyFiles(src_fs, selector, dst_fs, dst_path, arrow::io::default_io_context(), CHUNK_SIZE,
+                                       use_threads);
+    if (!status.ok()) {
+      throw std::runtime_error(status.ToString());
+    }
+    return true;
+  }
+
+ private:
+  bool continue_on_fail_;
+  arrow::fs::S3Options options_;
+  std::shared_ptr<arrow::fs::FileSystem> fs_;
+  std::shared_ptr<arrow::fs::FileSystem> src_s3fs_;
+  std::shared_ptr<arrow::fs::FileSystem> dst_s3fs_;
+
+  static arrow::fs::FileInfoVector GetFileInfos(std::shared_ptr<arrow::fs::FileSystem> fs,
+                                                const std::vector<std::string>& paths) {
+    if (paths.empty()) {
+      return {};
+    }
+
+    arrow::Result<arrow::fs::FileInfoVector> res = fs->GetFileInfo(paths);
+    if (!res.ok()) {
+      throw std::runtime_error(res.status().ToString());
+    }
+    return *res;
+  }
+
+  std::unordered_set<std::string> CheckFiles(const std::unordered_map<std::string, std::string>& renames) const {
+    std::unordered_set<std::string> found;
+    std::vector<std::string> src_s3_paths;
+    src_s3_paths.reserve(renames.size());
+
+    std::vector<std::string> src_local_paths;
+    src_local_paths.reserve(renames.size());
+
+    for (auto& [src_name, _] : renames) {
+      auto name = CropPrefix(src_name);
+      if (name == src_name) {
+        src_local_paths.push_back(name);
+      } else {
+        src_s3_paths.push_back(name);
+      }
+    }
+
+    for (auto& info : GetFileInfos(src_s3fs_, src_s3_paths)) {
+      if (info.IsFile()) {
+        found.insert(info.path());
+      }
+    }
+    for (auto& info : GetFileInfos(fs_, src_local_paths)) {
+      if (info.IsFile()) {
+        found.insert(info.path());
+      }
+    }
+    return found;
+  }
+
+  static void InitS3() {
+    if (!arrow::fs::IsS3Initialized()) {
+      arrow::fs::S3GlobalOptions global_options{};
+      global_options.log_level = arrow::fs::S3LogLevel::Error;
+      // global_options.log_level = arrow::fs::S3LogLevel::Debug;
+      if (!arrow::fs::InitializeS3(global_options).ok()) {
+        throw std::runtime_error("Cannot Initialize S3");
+      }
+    }
+  }
+
+  static arrow::fs::S3Options GetEnvOptions() {
+    auto options = arrow::fs::S3Options::Defaults();
+    options.endpoint_override = GetEnvVar(AWS_ENDPOINT_URL);
+    options.region = GetEnvVar(AWS_DEFAULT_REGION);
+    // options.scheme = "http";
+    options.ConfigureAccessKey(GetEnvVar(AWS_ACCESS_KEY_ID), GetEnvVar(AWS_SECRET_ACCESS_KEY));
+    return options;
+  }
+
+  static void ClearEnvOptions() {
+    setenv(AWS_EC2_METADATA_DISABLED, "true", 1);
+    unsetenv(AWS_ENDPOINT_URL);
+    unsetenv(AWS_DEFAULT_REGION);
+    unsetenv(AWS_ACCESS_KEY_ID);
+    unsetenv(AWS_SECRET_ACCESS_KEY);
+  }
+
+  static std::string GetEnvVar(const char* env) {
+    const char* value = getenv(env);
+    if (!value) {
+      throw std::runtime_error(std::string("env var not set '") + env + "'");
+    }
+    return value;
+  }
+
+  static std::string CropPrefix(const std::string& src, const std::string& prefix = "://") {
+    auto pos = src.find(prefix);
+    if (pos != std::string::npos) {
+      return src.substr(pos + prefix.size());
+    }
+    return src;
+  }
+};
+
+inline bool Rclone(const std::string& cmd, const std::string& arg1, const std::string& arg2) {
+  std::string sh_cmd = "rclone " + cmd + " " + arg1 + " " + arg2;
+  if (std::system(sh_cmd.data())) {
+    return false;
+  }
+  return true;
+}
+
+inline bool CopyDir(std::shared_ptr<S3Client> s3client, const std::string& src, const std::string& dst,
+                    bool use_threads = true) {
+  if (s3client) {
+    if (!s3client->CopyDir(src, dst, use_threads)) {
+      return false;
+    }
+  } else if (!Rclone("copy", src, dst)) {
+    return false;
+  }
+  return true;
+}
+
+inline bool CopyFiles(std::shared_ptr<S3Client> s3client, const std::unordered_map<std::string, std::string>& renames,
+                      bool use_threads = true) {
+  if (s3client) {
+    if (!s3client->CopyFiles(renames, use_threads)) {
+      return false;
+    }
+  } else {
+    for (auto& [src, dst] : renames) {
+      if (!Rclone("copyto", src, dst)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+}  // namespace ice_tea
