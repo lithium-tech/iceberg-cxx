80a25ac8994d1f7005c3fd20096688c6d80034ad
scanhex12 <vedernikovkostya@mail.ru>
2024-08-20 10:14:07 +0000

    Test to check compatibility of snapshot and uuid-generated filenames

--- tests/manifest_metadata_test.cpp
+++ tests/manifest_metadata_test.cpp
@@ -35,6 +35,34 @@ std::vector<ManifestFile> expected_list = {
                  .sequence_number = 3,
                  .snapshot_id = 7558608030923099867}};
 
+std::vector<ManifestFile> expected_list_uuid_generated = {
+    ManifestFile{.added_files_count = 6,
+                 .added_rows_count = 10000,
+                 .content = ManifestContent::kData,
+                 .deleted_files_count = 0,
+                 .deleted_rows_count = 0,
+                 .existing_files_count = 0,
+                 .existing_rows_count = 0,
+                 .length = 6825,
+                 .min_sequence_number = 2,
+                 .partition_spec_id = 0,
+                 .path = "s3://warehouse/gperov/test/metadata/98887eae-200b-1006-9bb5-0242ac110002-m0.avro",
+                 .sequence_number = 2,
+                 .snapshot_id = 5231658854638766100},
+    ManifestFile{.added_files_count = 1,
+                 .added_rows_count = 1,
+                 .content = ManifestContent::kDeletes,
+                 .deleted_files_count = 0,
+                 .deleted_rows_count = 0,
+                 .existing_files_count = 0,
+                 .existing_rows_count = 0,
+                 .length = 6714,
+                 .min_sequence_number = 3,
+                 .partition_spec_id = 0,
+                 .path = "s3://warehouse/gperov/test/metadata/98887ee4-200b-1006-9cd1-0242ac110002-m0.avro",
+                 .sequence_number = 3,
+                 .snapshot_id = 7558608030923099867}};
+
 void CheckEqual(const ManifestFile& m1, const ManifestFile& m2) {
   EXPECT_EQ(m1.path, m2.path);
   EXPECT_EQ(m1.length, m2.length);
@@ -74,6 +102,15 @@ TEST(Manifest, ReadWriteRead) {
   EXPECT_EQ(manifest_list.size(), 2);
   CheckEqual(manifest_list[0], expected_list[0]);
   CheckEqual(manifest_list[1], expected_list[1]);
+}
+
+TEST(Manifest, ReadFileUuidGenerator) {
+  std::ifstream input("metadata/snap-2dfe8bdb-200b-1006-9bb5-0242ac110002.avro");
+
+  std::vector<ManifestFile> manifest_list = ice_tea::ReadManifestList(input);
+  EXPECT_EQ(manifest_list.size(), 2);
+  CheckEqual(manifest_list[0], expected_list_uuid_generated[0]);
+  CheckEqual(manifest_list[1], expected_list_uuid_generated[1]);
 }
 
 }  // namespace iceberg
