2fdcecd86f588404d2f40805897d485ff7099fe0
gmusya <ggmusya@gmail.com>
2025-05-09 16:09:22 +0300

    Evaluate frequent items faster

--- stats/data.h
+++ stats/data.h
@@ -48,9 +48,19 @@ struct ParquetViewCommon {
   const int16_t* rep_levels;
 };
 
+struct ParquetViewDictionaryCommon : public ParquetViewCommon {
+  const int32_t* indices;
+  int32_t dictionary_length;
+};
+
 template <ParquetType parquet_type>
 struct ParquetBufferView : public ParquetViewCommon {
   const Traits<parquet_type>::PhysicalType* data;
+};
+
+template <ParquetType parquet_type>
+struct ParquetDictionaryBufferView : public ParquetViewDictionaryCommon {
+  const Traits<parquet_type>::PhysicalType* dictionary;
 };
 
 struct GenericParquetBufferView : public ParquetViewCommon {
@@ -58,21 +68,30 @@ struct GenericParquetBufferView : public ParquetViewCommon {
   ParquetType parquet_type;
 };
 
-template <ParquetType parquet_type>
-struct ParquetBuffer {
-  using Type = Traits<parquet_type>::PhysicalType;
+struct GenericParquetDictionaryBufferView : public ParquetViewDictionaryCommon {
+  const void* dictionary;
+  ParquetType parquet_type;
+};
 
+struct LevelsBuffer {
   int64_t values_size;
   int64_t levels_size;
   std::unique_ptr<Int16Data> def_levels;
   std::unique_ptr<Int16Data> rep_levels;
-  std::unique_ptr<Type[]> data;
 
-  explicit ParquetBuffer(int64_t batch_size) {
+  explicit LevelsBuffer(int64_t batch_size) {
     def_levels = std::make_unique<Int16Data>(batch_size);
     rep_levels = std::make_unique<Int16Data>(batch_size);
-    data = std::make_unique<Type[]>(batch_size);
   }
+};
+
+template <ParquetType parquet_type>
+struct ParquetBuffer : public LevelsBuffer {
+  using Type = Traits<parquet_type>::PhysicalType;
+
+  std::unique_ptr<Type[]> data;
+
+  explicit ParquetBuffer(int64_t batch_size) : LevelsBuffer(batch_size) { data = std::make_unique<Type[]>(batch_size); }
 
   ParquetBufferView<parquet_type> MakeView() {
     ParquetBufferView<parquet_type> view;
@@ -86,6 +105,32 @@ struct ParquetBuffer {
 };
 
 template <ParquetType parquet_type>
+struct ParquetDictionaryBuffer : public LevelsBuffer {
+  using Type = Traits<parquet_type>::PhysicalType;
+
+  const Type* dictionary_page;
+  std::unique_ptr<Int32Data> indices;
+  int32_t dictionary_length;
+
+  explicit ParquetDictionaryBuffer(int64_t batch_size) : LevelsBuffer(batch_size) {
+    indices = std::make_unique<Int32Data>(batch_size);
+  }
+
+  ParquetDictionaryBufferView<parquet_type> MakeView() {
+    ParquetDictionaryBufferView<parquet_type> view;
+    view.values_size = values_size;
+    view.levels_size = levels_size;
+    view.def_levels = def_levels.get();
+    view.rep_levels = rep_levels.get();
+    view.indices = indices.get();
+    view.dictionary_length = dictionary_length;
+    view.dictionary = dictionary_page;
+
+    return view;
+  }
+};
+
+template <ParquetType parquet_type>
 GenericParquetBufferView MakeGenericView(const ParquetBufferView<parquet_type>& view) {
   GenericParquetBufferView result;
   result.data = view.data;
@@ -94,6 +139,22 @@ GenericParquetBufferView MakeGenericView(const ParquetBufferView<parquet_type>& 
   result.values_size = view.values_size;
   result.levels_size = view.levels_size;
   result.parquet_type = parquet_type;
+  return result;
+}
+
+template <ParquetType parquet_type>
+GenericParquetDictionaryBufferView MakeGenericDictionaryView(const ParquetDictionaryBufferView<parquet_type>& view) {
+  GenericParquetDictionaryBufferView result;
+  result.def_levels = view.def_levels;
+  result.rep_levels = view.rep_levels;
+  result.values_size = view.values_size;
+  result.levels_size = view.levels_size;
+  result.parquet_type = parquet_type;
+
+  result.indices = view.indices;
+  result.dictionary = view.dictionary;
+  result.dictionary_length = view.dictionary_length;
+
   return result;
 }
 
@@ -127,10 +188,46 @@ class GenericParquetBuffer {
       buffer_;
 };
 
+class GenericParquetDictionaryBuffer {
+ public:
+  template <typename TypedBuffer>
+  explicit GenericParquetDictionaryBuffer(std::shared_ptr<TypedBuffer> buffer) {
+    buffer_.emplace<std::shared_ptr<TypedBuffer>>(buffer);
+  }
+
+  template <ParquetType parquet_type>
+  auto Get() const {
+    using BufferType = ParquetDictionaryBuffer<parquet_type>;
+
+    return std::get<std::shared_ptr<BufferType>>(buffer_);
+  }
+
+  GenericParquetDictionaryBufferView GetGenericView() const {
+    return std::visit(
+        [](auto&& arg) {
+          auto view = arg->MakeView();
+          return MakeGenericDictionaryView(view);
+        },
+        buffer_);
+  }
+
+ private:
+  std::variant<std::shared_ptr<ParquetDictionaryBuffer<ParquetType::INT32>>,
+               std::shared_ptr<ParquetDictionaryBuffer<ParquetType::INT64>>,
+               std::shared_ptr<ParquetDictionaryBuffer<ParquetType::BYTE_ARRAY>>,
+               std::shared_ptr<ParquetDictionaryBuffer<ParquetType::FIXED_LEN_BYTE_ARRAY>>>
+      buffer_;
+};
+
 using Int32ParquetBuffer = ParquetBuffer<ParquetType::INT32>;
 using Int64ParquetBuffer = ParquetBuffer<ParquetType::INT64>;
 using FLBAParquetBuffer = ParquetBuffer<ParquetType::FIXED_LEN_BYTE_ARRAY>;
 using ByteArrayParquetBuffer = ParquetBuffer<ParquetType::BYTE_ARRAY>;
+
+using Int32ParquetDictionaryBuffer = ParquetDictionaryBuffer<ParquetType::INT32>;
+using Int64ParquetDictionaryBuffer = ParquetDictionaryBuffer<ParquetType::INT64>;
+using FLBAParquetDictionaryBuffer = ParquetDictionaryBuffer<ParquetType::FIXED_LEN_BYTE_ARRAY>;
+using ByteArrayParquetDictionaryBuffer = ParquetDictionaryBuffer<ParquetType::BYTE_ARRAY>;
 
 template <ParquetType parquet_type>
 class ParquetReader {
@@ -153,6 +250,34 @@ class ParquetReader {
   std::shared_ptr<ParquetBuffer<parquet_type>> buffer_;
   int64_t batch_size_;
 };
+
+template <ParquetType parquet_type>
+class ParquetDictionaryReader {
+  using PhysicalType = Traits<parquet_type>::PhysicalType;
+  using ReaderType = Traits<parquet_type>::ReaderType;
+
+ public:
+  explicit ParquetDictionaryReader(std::shared_ptr<ReaderType> typed_reader,
+                                   std::shared_ptr<ParquetDictionaryBuffer<parquet_type>> buffer, int64_t batch_size)
+      : typed_reader_(typed_reader), buffer_(buffer), batch_size_(batch_size) {}
+
+  ParquetDictionaryBufferView<parquet_type> Read() {
+    buffer_->levels_size = typed_reader_->ReadBatchWithDictionary(
+        batch_size_, buffer_->def_levels.get(), buffer_->rep_levels.get(), buffer_->indices.get(),
+        &buffer_->values_size, &(buffer_->dictionary_page), &buffer_->dictionary_length);
+    return buffer_->MakeView();
+  }
+
+ private:
+  std::shared_ptr<ReaderType> typed_reader_;
+  std::shared_ptr<ParquetDictionaryBuffer<parquet_type>> buffer_;
+  int64_t batch_size_;
+};
+
+using Int32ParquetDictionaryReader = ParquetDictionaryReader<ParquetType::INT32>;
+using Int64ParquetDictionaryReader = ParquetDictionaryReader<ParquetType::INT64>;
+using FLBAParquetDictionaryReader = ParquetDictionaryReader<ParquetType::FIXED_LEN_BYTE_ARRAY>;
+using ByteArrayParquetDictionaryReader = ParquetDictionaryReader<ParquetType::BYTE_ARRAY>;
 
 using Int32ParquetReader = ParquetReader<ParquetType::INT32>;
 using Int64ParquetReader = ParquetReader<ParquetType::INT64>;
@@ -188,5 +313,26 @@ class GenericParquetReader {
 
   std::variant<std::shared_ptr<Int32ParquetReader>, std::shared_ptr<Int64ParquetReader>,
                std::shared_ptr<FLBAParquetReader>, std::shared_ptr<ByteArrayParquetReader>>
+      reader_;
+};
+
+class GenericParquetDictionaryReader {
+ public:
+  template <typename ParquetReader>
+  explicit GenericParquetDictionaryReader(std::shared_ptr<ParquetReader> reader) {
+    reader_.emplace<std::shared_ptr<ParquetReader>>(reader);
+  }
+
+  GenericParquetDictionaryBufferView ReadGeneric() {
+    return std::visit(
+        [](auto&& arg) {
+          auto res = arg->Read();
+          return MakeGenericDictionaryView(res);
+        },
+        reader_);
+  }
+
+  std::variant<std::shared_ptr<Int32ParquetDictionaryReader>, std::shared_ptr<Int64ParquetDictionaryReader>,
+               std::shared_ptr<FLBAParquetDictionaryReader>, std::shared_ptr<ByteArrayParquetDictionaryReader>>
       reader_;
 };
--- stats/datasketch/frequent_items.h
+++ stats/datasketch/frequent_items.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <stdexcept>
 #include <string>
 #include <utility>
+#include <variant>
 #include <vector>
 
 #include "frequent_items_sketch.hpp"
+#include "stats/datasketch/dictionary_serializer.h"
 #include "stats/types.h"
 
 namespace stats {
@@ -13,13 +16,18 @@ namespace stats {
 template <typename T>
 class FrequentItemsSketch {
  public:
+  using ValueType = T;
+
   static constexpr uint8_t kDefaultLgMaxMapSize = 4;
 
   explicit FrequentItemsSketch(uint8_t lg_max_map_size = kDefaultLgMaxMapSize) : sketch_(kDefaultLgMaxMapSize) {}
 
-  void AppendValue(const T& value) { sketch_.update(value); }
+  explicit FrequentItemsSketch(const datasketches::frequent_items_sketch<T>& other) : sketch_(other) {}
+  explicit FrequentItemsSketch(datasketches::frequent_items_sketch<T>&& other) : sketch_(std::move(other)) {}
 
-  std::vector<std::pair<T, double>> GetFrequentItems(int number_of_values) const {
+  void AppendValue(const T& value, int64_t weight = 1) { sketch_.update(value, weight); }
+
+  std::vector<std::pair<T, double>> GetFrequentItems() const {
     std::vector<std::pair<T, double>> result;
     const auto& freq_items = sketch_.get_frequent_items(datasketches::frequent_items_error_type::NO_FALSE_POSITIVES);
     result.reserve(freq_items.size());
@@ -31,6 +39,12 @@ class FrequentItemsSketch {
     return result;
   }
 
+  bool Empty() const { return sketch_.is_empty(); }
+
+  datasketches::frequent_items_sketch<T>& GetSketch() { return sketch_; }
+  const datasketches::frequent_items_sketch<T>& GetSketch() const { return sketch_; }
+
+ private:
   datasketches::frequent_items_sketch<T> sketch_;
 };
 
@@ -41,6 +55,9 @@ class GenericFrequentItemsSketch {
   explicit GenericFrequentItemsSketch(stats::Type type)
       : sketch_(type == stats::Type::kString ? VariantType(FrequentItemsSketch<std::string>())
                                              : VariantType(FrequentItemsSketch<int64_t>())) {}
+
+  explicit GenericFrequentItemsSketch(const VariantType& other) : sketch_(other) {}
+  explicit GenericFrequentItemsSketch(VariantType&& other) : sketch_(std::move(other)) {}
 
   template <typename T>
   void AppendValue(const T& value) {
@@ -53,10 +70,60 @@ class GenericFrequentItemsSketch {
   }
 
   template <typename T>
-  auto GetFrequentItems(int number_of_values) const {
-    return std::get<FrequentItemsSketch<T>>(sketch_).GetFrequentItems(number_of_values);
+  void AppendWeightedValue(const T& value, int64_t weight) {
+    std::get<FrequentItemsSketch<T>>(sketch_).AppendValue(value, weight);
   }
 
+  void AppendWeightedValue(const void* data, uint64_t size, int64_t weight) {
+    std::get<FrequentItemsSketch<std::string>>(sketch_).AppendValue(
+        std::string(reinterpret_cast<const char*>(data), size), weight);
+  }
+
+  template <typename T>
+  auto GetFrequentItems() const {
+    return std::get<FrequentItemsSketch<T>>(sketch_).GetFrequentItems();
+  }
+
+  bool Empty() const {
+    return std::visit([](auto&& arg) { return arg.Empty(); }, sketch_);
+  }
+
+  stats::Type Type() const {
+    if (std::holds_alternative<FrequentItemsSketch<std::string>>(sketch_)) {
+      return stats::Type::kString;
+    } else {
+      return stats::Type::kInt64;
+    }
+  }
+
+  template <typename DictionaryValue>
+  GenericFrequentItemsSketch FromDictionary(const IValuesProvider<int64_t, DictionaryValue>& dictionary_values) const {
+    if (!std::holds_alternative<FrequentItemsSketch<int64_t>>(sketch_)) {
+      throw std::runtime_error(std::string(__PRETTY_FUNCTION__) +
+                               ": trying to resolve dictionary in sketch, but sketch does not store integers");
+    }
+
+    const auto& typed_sketch = std::get<FrequentItemsSketch<int64_t>>(sketch_).GetSketch();
+    auto result = ResolveDictionary<int64_t, DictionaryValue>(typed_sketch, dictionary_values);
+
+    return GenericFrequentItemsSketch(FrequentItemsSketch<DictionaryValue>(std::move(result)));
+  }
+
+  void Merge(const GenericFrequentItemsSketch& other) {
+    std::visit(
+        [&]<typename ValueType>(FrequentItemsSketch<ValueType>& sketch) {
+          if (!std::holds_alternative<FrequentItemsSketch<ValueType>>(other.sketch_)) {
+            throw std::runtime_error(std::string(__PRETTY_FUNCTION__) +
+                                     ": merging sketches with different types is impossible");
+          }
+          const auto& raw_sketch = std::get<FrequentItemsSketch<ValueType>>(other.sketch_).GetSketch();
+
+          sketch.GetSketch().merge(raw_sketch);
+        },
+        sketch_);
+  }
+
+ private:
   VariantType sketch_;
 };
 
