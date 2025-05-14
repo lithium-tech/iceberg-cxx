15ecf2d7f39c02fbf849ab28d2b295c6bde3f508
gmusya <ggmusya@gmail.com>
2025-05-14 23:59:05 +0300

    Improve measuring code

--- stats/tests/run.sh
+++ stats/tests/run.sh
@@ -1,12 +1,15 @@
 # distinct
-time stats/stats_main --filename $FILEPATH --nouse_dictionary_optimization --evaluate_distinct --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/distinct_nodict.txt
-time stats/stats_main --filename $FILEPATH --use_dictionary_optimization --evaluate_distinct --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/distinct_dict.txt
+time stats/stats_main --filename $FILEPATH --nouse_dictionary_optimization --nouse_string_view_heuristic --evaluate_distinct --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/distinct.txt
+time stats/stats_main --filename $FILEPATH --nouse_dictionary_optimization --use_string_view_heuristic --evaluate_distinct --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/distinct_sv.txt
+time stats/stats_main --filename $FILEPATH --use_dictionary_optimization --nouse_string_view_heuristic --evaluate_distinct --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/distinct_dict.txt
+time stats/stats_main --filename $FILEPATH --use_dictionary_optimization --use_string_view_heuristic --evaluate_distinct --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/distinct_dict_sv.txt
 
 # frequent items
-time stats/stats_main --filename $FILEPATH --nouse_precalculation_optimization --nouse_dictionary_optimization --evaluate_frequent_items --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/fi_nodict.txt
-time stats/stats_main --filename $FILEPATH --nouse_precalculation_optimization --use_dictionary_optimization --evaluate_frequent_items --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/fi_dict_noprecalc.txt
-time stats/stats_main --filename $FILEPATH --use_precalculation_optimization --use_dictionary_optimization --evaluate_frequent_items --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/fi_dict_precalc.txt
+time stats/stats_main --filename $FILEPATH --nouse_dictionary_optimization --nouse_precalculation_optimization --nouse_string_view_heuristic --evaluate_frequent_items --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/fi.txt
+time stats/stats_main --filename $FILEPATH --nouse_dictionary_optimization --nouse_precalculation_optimization --nouse_string_view_heuristic --evaluate_frequent_items --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/fi_nodict.txt
+time stats/stats_main --filename $FILEPATH --use_dictionary_optimization --use_precalculation_optimization --use_string_view_heuristic --evaluate_frequent_items --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/fi_dict_prec.txt
 
 # quantiles
-time stats/stats_main --filename $FILEPATH --nouse_precalculation_optimization --nouse_dictionary_optimization --evaluate_quantiles --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/quantiles_nodict.txt
-time stats/stats_main --filename $FILEPATH --use_precalculation_optimization --use_dictionary_optimization --evaluate_quantiles --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/quantiles_dict_precalc_sv.txt
+time stats/stats_main --filename $FILEPATH --nouse_dictionary_optimization --nouse_precalculation_optimization --nouse_string_view_heuristic --evaluate_quantiles --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/quantiles.txt
+time stats/stats_main --filename $FILEPATH --use_dictionary_optimization --use_precalculation_optimization --use_string_view_heuristic --evaluate_quantiles --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/quantiles_dict_prec_sv.txt
+time stats/stats_main --filename $FILEPATH --use_dictionary_optimization --use_precalculation_optimization --use_string_view_heuristic --evaluate_quantiles --print_timings 2>&1 | tee $CURRENT_RESULTS_DIR/quantiles_dict_prec_sv_patched.txt
