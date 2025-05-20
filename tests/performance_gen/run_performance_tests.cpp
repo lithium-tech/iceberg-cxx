47e137e9ee5147fb0c2b875ffc141a612a93fac4
Artem Zuikov <chertus@gmail.com>
2025-05-20 12:23:42 +0300

    Add config to GetScanMetadata (#27)
    Co-authored-by: BlackAntik <antonblack179179179@gmail.com>

--- tests/performance_gen/run_performance_tests.cpp
+++ tests/performance_gen/run_performance_tests.cpp
@@ -90,12 +90,14 @@ class LoggingFileSystem : public FileSystemWrapper {
   std::shared_ptr<Metrics> metrics_;
 };
 
-void run_manifest_entries(std::shared_ptr<arrow::fs::FileSystem> fs, const int times) {
+void run_manifest_entries(std::shared_ptr<arrow::fs::FileSystem> fs, const int times,
+                          const iceberg::ice_tea::GetScanMetadataConfig& config) {
   for (int _ = 0; _ < times; ++_) {
     auto maybe_scan_metadata =
         iceberg::ice_tea::GetScanMetadata(fs,
                                           "s3://warehouse/performance/manifest_entries/metadata/"
-                                          "00001-3e9c137d-3f3a-4d9f-ad3c-abd4911f4c9f.metadata.json");
+                                          "00001-3e9c137d-3f3a-4d9f-ad3c-abd4911f4c9f.metadata.json",
+                                          config);
     if (maybe_scan_metadata.status() != arrow::Status::OK()) {
       std::cerr << "run_manifest_entries failed" << std::endl;
       exit(1);
@@ -103,11 +105,13 @@ void run_manifest_entries(std::shared_ptr<arrow::fs::FileSystem> fs, const int t
   }
 }
 
-void run_manifest_files(std::shared_ptr<arrow::fs::FileSystem> fs, const int times) {
+void run_manifest_files(std::shared_ptr<arrow::fs::FileSystem> fs, const int times,
+                        const iceberg::ice_tea::GetScanMetadataConfig& config) {
   for (int _ = 0; _ < times; ++_) {
     auto maybe_scan_metadata = iceberg::ice_tea::GetScanMetadata(
         fs,
-        "s3://warehouse/performance/manifest_files/metadata/00002-37c508a5-8a06-4823-845e-889dff066f72.metadata.json");
+        "s3://warehouse/performance/manifest_files/metadata/00002-37c508a5-8a06-4823-845e-889dff066f72.metadata.json",
+        config);
     if (maybe_scan_metadata.status() != arrow::Status::OK()) {
       std::cerr << "run_manifest_files failed" << std::endl;
       exit(1);
@@ -115,10 +119,12 @@ void run_manifest_files(std::shared_ptr<arrow::fs::FileSystem> fs, const int tim
   }
 }
 
-void run_schemas(std::shared_ptr<arrow::fs::FileSystem> fs, const int times) {
+void run_schemas(std::shared_ptr<arrow::fs::FileSystem> fs, const int times,
+                 const iceberg::ice_tea::GetScanMetadataConfig& config) {
   for (int _ = 0; _ < times; ++_) {
     auto maybe_scan_metadata = iceberg::ice_tea::GetScanMetadata(
-        fs, "s3://warehouse/performance/schemas/metadata/00868-7a2e9a74-46be-4dd4-a81a-771445e15034.metadata.json");
+        fs, "s3://warehouse/performance/schemas/metadata/00868-7a2e9a74-46be-4dd4-a81a-771445e15034.metadata.json",
+        config);
     if (maybe_scan_metadata.status() != arrow::Status::OK()) {
       std::cerr << "run_schemas failed" << std::endl;
       exit(1);
@@ -126,10 +132,12 @@ void run_schemas(std::shared_ptr<arrow::fs::FileSystem> fs, const int times) {
   }
 }
 
-void run_snapshots(std::shared_ptr<arrow::fs::FileSystem> fs, const int times) {
+void run_snapshots(std::shared_ptr<arrow::fs::FileSystem> fs, const int times,
+                   const iceberg::ice_tea::GetScanMetadataConfig& config) {
   for (int _ = 0; _ < times; ++_) {
     auto maybe_scan_metadata = iceberg::ice_tea::GetScanMetadata(
-        fs, "s3://warehouse/performance/snapshots/metadata/01999-084000f2-7dd9-4e7c-adfd-24ec13d717c0.metadata.json");
+        fs, "s3://warehouse/performance/snapshots/metadata/01999-084000f2-7dd9-4e7c-adfd-24ec13d717c0.metadata.json",
+        config);
     if (maybe_scan_metadata.status() != arrow::Status::OK()) {
       std::cerr << "run_snapshots failed" << std::endl;
       exit(1);
@@ -142,6 +150,7 @@ ABSL_FLAG(int, manifest_entries, 0, "how many times manifest entries performance
 ABSL_FLAG(int, manifest_files, 0, "how many times manifest files performance test will run");
 ABSL_FLAG(int, schemas, 0, "how many times schemas performance test will run");
 ABSL_FLAG(int, snapshots, 0, "how many times snapshots performance test will run");
+ABSL_FLAG(bool, full, true, "if true, GetScanMetadata claims all metadata");
 
 int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
@@ -149,15 +158,27 @@ int main(int argc, char* argv[]) {
   const int manifest_files = absl::GetFlag(FLAGS_manifest_files);
   const int schemas = absl::GetFlag(FLAGS_schemas);
   const int snapshots = absl::GetFlag(FLAGS_snapshots);
+  const bool full = absl::GetFlag(FLAGS_full);
 
   std::shared_ptr<Metrics> metrics = std::make_shared<Metrics>(Metrics{0, 0, 0});
 
   std::shared_ptr<arrow::fs::FileSystem> fs = std::make_shared<arrow::fs::LocalFileSystem>();
   fs = std::make_shared<LoggingFileSystem>(fs, metrics);
-  run_manifest_entries(fs, manifest_entries);
-  run_manifest_files(fs, manifest_files);
-  run_schemas(fs, schemas);
-  run_snapshots(fs, snapshots);
+
+  iceberg::ice_tea::GetScanMetadataConfig config;
+
+  if (!full) {
+    config.manifest_entry_deserializer_config.datafile_config.extract_column_sizes = false;
+    config.manifest_entry_deserializer_config.datafile_config.extract_value_counts = false;
+    config.manifest_entry_deserializer_config.datafile_config.extract_null_value_counts = false;
+    config.manifest_entry_deserializer_config.datafile_config.extract_nan_value_counts = false;
+    config.manifest_entry_deserializer_config.datafile_config.extract_distinct_counts = false;
+  }
+
+  run_manifest_entries(fs, manifest_entries, config);
+  run_manifest_files(fs, manifest_files, config);
+  run_schemas(fs, schemas, config);
+  run_snapshots(fs, snapshots, config);
   std::cerr << "FileSystem report: requests: " << metrics->requests << std::endl;
   std::cerr << "FileSystem report: bytes read: " << metrics->bytes_read << std::endl;
   std::cerr << "FileSystem report: files opened: " << metrics->files_opened << std::endl;
