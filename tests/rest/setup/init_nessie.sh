5ed2115b149ce629c4b6d79abd2a80369b59309a
Artem Zuikov <chertus@gmail.com>
2025-06-11 17:23:58 +0300

    Update init_nessie.sh

--- tests/rest/setup/init_nessie.sh
+++ tests/rest/setup/init_nessie.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pip3 install --upgrade --break-system-packages pynessie "marshmallow<4" flask
 wget --no-verbose https://github.com/projectnessie/nessie/releases/download/nessie-0.104.1/nessie-quarkus-0.104.1-runner.jar
 nohup java -jar nessie-quarkus-0.104.1-runner.jar > nessie.log 2>&1 &
 sleep 20
