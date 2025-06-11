4e5041917d0f7243cb10ed4c0191cf03c5e838a8
Artem Zuikov <chertus@gmail.com>
2025-06-11 17:00:55 +0300

    Update init_nessie.sh

--- tests/rest/setup/init_nessie.sh
+++ tests/rest/setup/init_nessie.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+wget https://github.com/projectnessie/nessie/releases/download/nessie-0.104.1/nessie-quarkus-0.104.1-runner.jar
 nohup java -jar nessie-quarkus-0.104.1-runner.jar > nessie.log 2>&1 &
 sleep 20
