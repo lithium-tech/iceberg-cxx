8c654fe1194781609aaa8f6052e6998fc05bf455
Anton Cherniy <antonblack179179179@gmail.com>
2025-06-27 13:14:12 +0300

    reuse FileSystemWrapper

--- iceberg/positional_delete/ut/positional_delete_test.cpp
+++ iceberg/positional_delete/ut/positional_delete_test.cpp
@@ -1,4 +1,5 @@
 #include "iceberg/positional_delete/positional_delete.h"
+#include "iceberg/common/fs/filesystem_wrapper.h"
 
 #include <memory>
 #include <string>
@@ -176,32 +177,6 @@ struct Metrics {
   uint64_t files_opened = 0;
 
   bool operator==(const Metrics& other) const = default;
-};
-
-class InputFileWrapper : public arrow::io::RandomAccessFile {
- public:
-  explicit InputFileWrapper(std::shared_ptr<arrow::io::RandomAccessFile> file) : file_(file) {}
-
-  arrow::Result<int64_t> ReadAt(int64_t position, int64_t nbytes, void* out) override {
-    return file_->ReadAt(position, nbytes, out);
-  }
-
-  arrow::Status Close() override { return file_->Close(); }
-
-  bool closed() const override { return file_->closed(); }
-
-  arrow::Result<int64_t> Tell() const override { return file_->Tell(); }
-
-  arrow::Result<int64_t> Read(int64_t nbytes, void* out) override { return file_->Read(nbytes, out); }
-
-  arrow::Result<std::shared_ptr<arrow::Buffer>> Read(int64_t nbytes) override { return file_->Read(nbytes); }
-
-  arrow::Status Seek(int64_t position) override { return file_->Seek(position); }
-
-  arrow::Result<int64_t> GetSize() override { return file_->GetSize(); }
-
- private:
-  std::shared_ptr<arrow::io::RandomAccessFile> file_;
 };
 
 class LoggingInputFile : public InputFileWrapper {
