aa1902f82df9a6d3125c1d5d958a25741282d414
Anton Cherniy <antonblack179179179@gmail.com>
2025-07-10 19:42:06 +0300

    add field_id_first test

--- iceberg/streams/ut/file_reader_builder_test.cpp
+++ iceberg/streams/ut/file_reader_builder_test.cpp
@@ -98,6 +98,38 @@ TEST_F(FileReaderBuilderTest, SchemaNameMapping) {
       *MakeBatch({MakeInt32ArrowColumn(col1_data), MakeInt32ArrowColumn(col2_data)}, {"col1", "col2"})));
 }
 
+TEST_F(FileReaderBuilderTest, FieldIdFirst) {
+  std::vector<int> field_ids_to_retrieve = {1};
+
+  auto field_id_mapper = std::make_shared<FieldIdMapper>(std::map<int, std::string>{{1, "col1"}});
+
+  const std::string schema_name_mapping =
+      "[ { \"field-id\": 1, \"names\": [\"yyy\"] },"
+      " { \"field-id\": 2, \"names\": [\"xxx\"] } ]";
+
+  FileReaderBuilder file_reader_builder(field_ids_to_retrieve, equality_deletes_, field_id_mapper,
+                                        std::make_shared<FileReaderProvider>(fs_provider_), nullptr,
+                                        &schema_name_mapping);
+
+  auto col1_data = OptionalVector<int32_t>{1, 2, 3, 4};
+  auto col2_data = OptionalVector<int32_t>{5, 6, 7, 8};
+
+  auto column1 = MakeInt32Column("xxx", 1, col1_data);
+  auto column2 = MakeInt32Column("yyy", 2, col2_data);
+  ASSERT_OK(WriteToFile({column1, column2}, data_path_));
+
+  PartitionLayerFile state(PartitionLayer(13, 12), data_path_);
+
+  AnnotatedDataPath annotated_data_path(state, {AnnotatedDataPath::Segment{.offset = 4, .length = 1}});
+
+  auto reader = file_reader_builder.Build(annotated_data_path);
+
+  auto batch = reader->ReadNext();
+  ASSERT_TRUE(batch);
+
+  EXPECT_TRUE(batch->GetRecordBatch()->Equals(*MakeBatch({MakeInt32ArrowColumn(col1_data)}, {"col1"})));
+}
+
 TEST_F(FileReaderBuilderTest, SchemaNameMappingMissingColumn) {
   std::vector<int> field_ids_to_retrieve = {1, 2};
 
