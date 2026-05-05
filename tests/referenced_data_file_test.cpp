8eee50570f1ae255b9894177e4880329ad22578e
Мощенский Михаил <92210552+MeT3ger@users.noreply.github.com>
2026-05-05 20:59:53 +0300

    fix: scan-metadata-posdeletes optimization (#145)
    Optimization was always applied, regardless of this parameter
    
    ---------
    
    Co-authored-by: MeT3ger <pudgekrut12345>

--- tests/referenced_data_file_test.cpp
+++ tests/referenced_data_file_test.cpp
@@ -188,5 +188,100 @@ TEST(ReferencedDataFile, GetScanMetadata) {
   EXPECT_EQ(parts[1][0].positional_delete_entries_[0].path, "delete2");
 }
 
+ManifestEntry MakePositionalDeleteEntryUnannotated(const std::string& delete_path) {
+  ManifestEntry entry;
+  entry.status = ManifestEntry::Status::kAdded;
+  entry.data_file.content = ContentFile::FileContent::kPositionDeletes;
+  entry.data_file.file_format = "PARQUET";
+  entry.data_file.file_path = delete_path;
+  entry.data_file.record_count = 1;
+  entry.data_file.file_size_in_bytes = 123;
+  return entry;
+}
+
+TEST(ReferencedDataFile, GetScanMetadataUnannotatedDeleteOptimizationDisabled) {
+  ScopedTempDir dir;
+
+  {
+    Manifest manifest;
+    {
+      manifest.entries.emplace_back(MakeDataEntry("data1"));
+      manifest.entries.emplace_back(MakeDataEntry("data2"));
+      manifest.entries.emplace_back(MakePositionalDeleteEntry("delete1", "data1"));
+      manifest.entries.emplace_back(MakePositionalDeleteEntry("delete2", "data2"));
+      manifest.entries.emplace_back(MakePositionalDeleteEntryUnannotated("delete3"));
+    }
+
+    std::string data = ice_tea::WriteManifestEntries(manifest);
+    auto result = ice_tea::ReadManifestEntries(data, {});
+    std::ofstream f(dir.path() / "manifest.avro", std::ios_base::binary);
+    f.write(data.data(), data.size());
+    f.close();
+  }
+
+  {
+    ManifestFile manifest_file;
+    manifest_file.added_files_count = 1;
+    manifest_file.added_rows_count = 1;
+    manifest_file.partition_spec_id = 1;
+    manifest_file.path = "file://" + (dir.path() / "manifest.avro").string();
+
+    std::string data = ice_tea::WriteManifestList({manifest_file});
+
+    std::ofstream f(dir.path() / "manifest_list.avro", std::ios_base::binary);
+    f.write(data.data(), data.size());
+    f.close();
+  }
+
+  {
+    TableMetadataV2Builder builder;
+    builder.table_uuid = "q";
+    builder.location = "loc";
+    builder.last_sequence_number = 1;
+    builder.last_updated_ms = 1;
+    builder.last_column_id = 10;
+    builder.schemas = {
+        std::make_shared<iceberg::Schema>(1, std::vector<types::NestedField>{types::NestedField{
+                                                 .name = "c1",
+                                                 .field_id = 1,
+                                                 .is_required = false,
+                                                 .type = std::make_shared<types::PrimitiveType>(TypeID::kInt)}})};
+    builder.current_schema_id = 1;
+    builder.partition_specs = {
+        std::make_shared<iceberg::PartitionSpec>(iceberg::PartitionSpec{.spec_id = 1, .fields = {}})};
+    builder.default_spec_id = 1;
+    builder.last_partition_id = 1;
+    builder.sort_orders = {std::make_shared<iceberg::SortOrder>(SortOrder{.order_id = 1, .fields = {}})};
+    builder.default_sort_order_id = 1;
+    builder.snapshots = {std::make_shared<Snapshot>(
+        Snapshot{.snapshot_id = 1,
+                 .parent_snapshot_id = std::nullopt,
+                 .sequence_number = 1,
+                 .timestamp_ms = 1,
+                 .manifest_list_location = "file://" + (dir.path() / "manifest_list.avro").string(),
+                 .summary = std::map<std::string, std::string>{{"operation", "append"}},
+                 .schema_id = 1})};
+    builder.current_snapshot_id = 1;
+    auto table_metadata = builder.Build();
+    ASSERT_TRUE(table_metadata != nullptr);
+
+    std::string data = ice_tea::WriteTableMetadataV2(*table_metadata);
+
+    std::ofstream f(dir.path() / "snapshot.json", std::ios_base::binary);
+    f.write(data.data(), data.size());
+    f.close();
+  }
+
+  auto fs = std::make_shared<arrow::fs::LocalFileSystem>();
+  std::string snapshot_path = "file://" + (dir.path() / "snapshot.json").string();
+  auto s = ice_tea::GetScanMetadata(fs, snapshot_path, [](iceberg::Schema& schema) { return true; });
+
+  ASSERT_EQ(s.status(), arrow::Status::OK());
+  ASSERT_EQ(s->partitions.size(), 1);  // Most important check
+  ASSERT_EQ(s->partitions[0].size(), 1);
+  ASSERT_EQ(s->partitions[0][0].data_entries_.size(), 2);
+  ASSERT_EQ(s->partitions[0][0].positional_delete_entries_.size(), 3);
+}
+
 }  // namespace
 }  // namespace iceberg
